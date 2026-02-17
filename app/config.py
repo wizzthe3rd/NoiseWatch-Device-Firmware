ec0bd5bc71020606e0b17dd638c50510d778f466
wizzthe3rd <wesleyosayande@gmail.com>
2026-02-17 15:55:55 +0000

    refactor

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ import os
 
 load_dotenv()
 
-DEVICE_ID = os.get_env("DEVICE_ID")
-DEVICE_TOKEN = os.get_env("DEVICE_TOKEN")
-API_URL = os.get_env("API_URL")
-PUSH_INTERVAL = os.getenv("PUSH_INTERVAL")  # interval between noise requests sent to api
+DEVICE_ID = os.getenv("DEVICE_ID")
+DEVICE_TOKEN = os.getenv("DEVICE_TOKEN")
+API_URL = os.getenv("API_URL")
+PUSH_INTERVAL = os.getenv("PUSH_INTERVAL")  # interval between noise requests sent to api in seconds
 DECIBEL_OFFSET = os.getenv("DECIBEL_OFFSET")  # added to readings to make decibel vals more readable
 
 
